5e13729a3737da501c35d0557dfd5ef20ba7b1ec
Andrey S <norwegianwood409@gmail.com>
2021-03-07 13:09:22 +0500

    Init

---
+++ src/sec_hw1/Main.java
@@ -0,0 +1,24 @@
+package sec_hw1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static void prepare() {
+
+    }
+
+    public static void encode() {
+
+    }
+
+    public static void translate() {
+
+    }
+
+    public static void decode() {
+
+    }
+}
